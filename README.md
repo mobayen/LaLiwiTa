3945a00fd1b62fb2b2b4b331fee188418477f0b7
MReza Mobayen <mobayen.mreza@gmail.com>
2020-11-28 22:51:49 -0600

    requires spatie laravel permissions

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 ## My customized version of Laravel
 
 Laravel + Livewire + tailwind css
+
+## Packages
+
+### Spatie: Laravel permissions
+
+documentation: https://spatie.be/docs/laravel-permission/v3/introduction
+repository: https://github.com/spatie/laravel-permission
--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,8 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Jetstream\HasTeams;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasPermissions;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
@@ -18,6 +20,8 @@ class User extends Authenticatable
     use HasTeams;
     use Notifiable;
     use TwoFactorAuthenticatable;
+    use HasRoles;
+    use HasPermissions;
 
     /**
      * The attributes that are mass assignable.
